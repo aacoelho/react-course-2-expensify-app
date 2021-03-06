83de8eee9d0e92e45d8ae5445e4c050892b70a88
aacoelho93 <angacoelho@gmail.com>
2021-03-06 21:25:00 +0000

    Handle data of firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,31 +15,106 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-	name: 'Angela Coelho',
-	age: 26,
-	isSingle: true,
-	location: {
-		city: 'Coimbra',
-		country: 'Portugal'
-	}
-}).then(() => {
-	console.log('Data is saved.');
-}).catch((e) => {
-	console.log('This failed.', e);
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+	console.log(snapshot.key, snapshot.val());
 });
 
-// database.ref().set('This is my data.');
-
-// database.ref('age').set(27);
-// database.ref('location/city').set('Porto');
-
-database.ref('attributes').set({
-	height: 158,
-	weight: 60
-}).then(() => {
-	console.log('Second set call worked.');
-}).catch((e) => {
-	console.log('Things didnt for the second error.', e);
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+	console.log(snapshot.key, snapshot.val());
 });
 
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+	console.log(snapshot.key, snapshot.val());
+});
+
+// database.ref('expenses').on('value', (snapshot) => {
+// 	const expenses = [];
+
+// 	snapshot.forEach((childSnapshot) => {
+// 		expenses.push({
+// 			id: childSnapshot.key,
+// 			...childSnapshot.val()
+// 		})
+// 	});
+
+// 	console.log(expenses);
+// });
+
+// database.ref('expenses')
+// 	.once('value')
+// 	.then((snapshot) => {
+// 		const expenses = [];
+
+// 		snapshot.forEach((childSnapshot) => {
+// 			expenses.push({
+// 				id: childSnapshot.key,
+// 				...childSnapshot.val()
+// 			})
+// 		});
+
+// 		console.log(expenses);
+// 	});
+
+// database.ref('expenses').push({
+// 	description: 'Rent',
+// 	note: '',
+// 	amount: 109500,
+// 	createAt: 976123498763
+// });
+
+// database.ref('notes/-MV8Kn4rjJUAuK8fASat').remove();
+
+// database.ref('notes').push({
+// 	title: 'Course Topics',
+// 	body: 'React Native, Angular, Python'
+// });
+
+// database.ref().on('value', (snapshot) => {
+// 	const val = snapshot.val();
+// 	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}.`);
+// });
+
+// database.ref('location/city')
+// 	.once('value')
+// 	.then((snapshot) => {
+// 		const val = snapshot.val();
+// 		console.log(val);
+// 	})
+// 	.catch((e) => {
+// 		console.log('Error fetching data', e);
+// 	});
+
+// database.ref().set({
+// 	name: 'Angela Coelho',
+// 	age: 27,
+// 	stressLevel: 6,
+// 	job: {
+// 		title: 'Frontend developer',
+// 		company: 'Google'
+// 	},
+// 	location: {
+// 		city: 'Coimbra',
+// 		country: 'Portugal'
+// 	}
+// }).then(() => {
+// 	console.log('Data is saved.');
+// }).catch((e) => {
+// 	console.log('This failed.', e);
+// });
+
+// database.ref().update({
+// 	stressLevel: 9,
+// 	'job/company': 'Amazon',
+// 	'location/city': 'Porto'
+// });
+
+// database.ref()
+// 	.remove()
+// 	.then(() => {
+// 		console.log('Data was removed.');
+// 	}).catch((e) => {
+// 		console.log('Did not remove data', e);
+// 	});
